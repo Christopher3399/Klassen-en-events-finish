51b3e98b04f569da12c5306aa15735f757e48ae0
Chris <christopher.abdallah.ebwa@student.ehb.be>
2023-10-09 19:46:58 +0200

    finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Klassen_en_events_finish
+{
+    
+    public class Boek
+    {
+        public string Isbn { get; set; }
+        public string Naam { get; set; }
+        public string Uitgever { get; set; }
+        public double Prijs { get; set; }
+
+        public Boek(string isbn, string naam, string uitgever, double prijs)
+        {
+            Isbn = isbn;
+            Naam = naam;
+            Uitgever = uitgever;
+            Prijs = prijs;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {Isbn}\nNaam: {Naam}\nUitgever: {Uitgever}\nPrijs: {Prijs:C}";
+        }
+
+        public void Lees()
+        {
+            Console.WriteLine($"Je leest het boek '{Naam}' door {Uitgever}.");
+        }
+    }
+
+    // Enum pour la périodicité du tijdschrift
+    public enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
+
+    public class Tijdschrift : Boek
+    {
+        public Verschijningsperiode Periode { get; set; }
+
+        public Tijdschrift(string isbn, string naam, string uitgever, double prijs, Verschijningsperiode periode)
+            : base(isbn, naam, uitgever, prijs)
+        {
+            Periode = periode;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\nVerschijningsperiode: {Periode}";
+        }
+
+        public new void Lees()
+        {
+            Console.WriteLine($"Je leest het tijdschrift '{Naam}' ({Periode}) door {Uitgever}.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création d'instances de Boek
+            Boek boek1 = new Boek("978-3-16-148410-0", "C# Programming", "Tech Publications", 29.99);
+            Boek boek2 = new Boek("978-1-23-456789-0", "Python Basics", "Code Experts", 19.99);
+
+            // Création d'instances de Tijdschrift
+            Tijdschrift tijdschrift1 = new Tijdschrift("978-9-87-654321-0", "Tech News", "Tech World", 5.99, Verschijningsperiode.Wekelijks);
+            Tijdschrift tijdschrift2 = new Tijdschrift("978-0-12-345678-9", "Science Digest", "Science Publications", 6.99, Verschijningsperiode.Maandelijks);
+
+            // Affichage des détails
+            Console.WriteLine("Détails du Boek 1 :");
+            Console.WriteLine(boek1);
+
+            Console.WriteLine("\nDétails du Tijdschrift 1 :");
+            Console.WriteLine(tijdschrift1);
+
+            // Simulation de la lecture
+            Console.WriteLine("\nSimulation de la lecture :");
+            boek1.Lees();
+            tijdschrift1.Lees();
+        }
+    }
+
+
+}
---
+++ bestelling.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_en_events_finish
+{
+    public class Bestelling<T>
+    {
+        private static int volgnummerCount = 1; // Compteur de volgnummers pour les IDs uniques
+        private int volgnummer; // ID unique de la commande
+
+        public int Id
+        {
+            get { return volgnummer; }
+            private set { volgnummer = value; }
+        }
+
+        public T Item { get; set; }
+        public DateTime Datum { get; set; }
+        public int Aantal { get; set; }
+        public Verschijningsperiode? Periode { get; set; } // Période de la commande (tijdschriften-abonnement)
+
+        public Bestelling(T item, DateTime datum, int aantal, Verschijningsperiode? periode = null)
+        {
+            Id = volgnummerCount++; // Attribuer un ID unique à la commande
+            Item = item;
+            Datum = datum;
+            Aantal = aantal;
+            Periode = periode;
+        }
+
+        // Getter-Setter pour la propriété Prix pour s'assurer que le prix est entre 5€ et 50€
+        private double prijs;
+        public double Prijs
+        {
+            get { return prijs; }
+            set
+            {
+                if (value >= 5 && value <= 50)
+                {
+                    prijs = value;
+                }
+                else
+                {
+                    Console.WriteLine("Le prix doit être entre 5€ et 50€.");
+                }
+            }
+        }
+
+        // Méthode pour passer une commande et retourner une Tuple avec les informations
+        public Tuple<string, int, double> Bestel()
+        {
+            string isbn = "";
+            int aantalBesteld = 0;
+
+            if (Item is Boek boek)
+            {
+                isbn = boek.Isbn;
+                aantalBesteld = Aantal;
+                Prijs = boek.Prijs * Aantal;
+                OnBestellingGeplaatst("Boek besteld");
+            }
+            else if (Item is Tijdschrift tijdschrift)
+            {
+                isbn = tijdschrift.Isbn;
+                aantalBesteld = Aantal;
+                Prijs = tijdschrift.Prijs * Aantal;
+                OnBestellingGeplaatst("Tijdschrift besteld");
+            }
+
+            return Tuple.Create(isbn, aantalBesteld, Prijs);
+        }
+
+        // Event pour confirmer la commande
+        public event Action<string> BestellingGeplaatst;
+
+        protected virtual void OnBestellingGeplaatst(string message)
+        {
+            BestellingGeplaatst?.Invoke(message);
+        }
+    }
+}
